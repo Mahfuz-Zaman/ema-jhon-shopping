28adbfd61f9e4f7543cd8fe6a562408763aeb911
Mahfuz-Zaman <zaman.13121996@.com>
2021-09-24 20:26:10 +0600

    updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,20 +3,26 @@ import './Cart.css'
 
 const Cart = (props) => {
     const { cart } = props;
+    let totalQuantity = 0;
     let total = 0;
     for (const product of cart) {
-        total = total + product.price;
+        if (!product.quantity) {
+            product.quantity = 1;
+        }
+        total = total + product.price * product.quantity;
+        totalQuantity = totalQuantity + product.quantity;
     }
 
-    const shipping = 50;
-    const tax = (total) * .10;
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * .10;
     const grandTotal = total + shipping + tax;
+
 
 
     return (
         <div className="cart">
             <h2>Order Summary</h2>
-            <h3>cart Items: {props.cart.length}</h3>
+            <h3>cart Items: {totalQuantity}</h3>
             <br />
             <p>Total: {total.toFixed(2)}</p>
             <p>Shipping: {shipping}</p>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,9 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 import './Product.css'
+import Rating from 'react-rating';
 
 const Product = (props) => {
-    const { name, img, seller, price, stock } = props.product
+    const { name, img, seller, price, stock, star } = props.product
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
@@ -17,6 +18,12 @@ const Product = (props) => {
                 <p><small>By: {seller}</small></p>
                 <h3>Price: ${price}</h3>
                 <p>only {stock} left in stock - order soon</p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly></Rating>
+                <br />
                 <button onClick={() => props.handleAddToCart(props.product)}
                     className="product-btn">{cartIcon} Add To Cart</button>
             </div>
